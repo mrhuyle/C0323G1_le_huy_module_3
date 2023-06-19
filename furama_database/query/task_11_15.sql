d396455b11b6cf1b1d4d6399bd7eabf6b1d18227
Le Huy <lehuy@Les-MacBook-Air.local>
2023-06-19 19:42:44 +0700

    11 to 15

--- furama_database/query/task_11_15.sql
+++ furama_database/query/task_11_15.sql
@@ -51,5 +51,44 @@ FROM details_contract
 GROUP BY details_contract.additional_service_code
 ) result
 ON details_contract.details_contract_code = result.code);
-        
-        
+
+-- 14. Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
+
+SELECT	contract.contract_code,
+		service_type.service_type_name,
+        result.name,
+        result.count
+FROM	
+(SELECT	additional_service.additional_service_name AS name,
+        additional_service.additional_service_code AS code,
+        count(distinct additional_service.additional_service_code) AS count
+FROM	details_contract
+JOIN	additional_service ON details_contract.additional_service_code = additional_service.additional_service_code
+GROUP BY
+    additional_service.additional_service_code
+HAVING
+    COUNT(additional_service.additional_service_code) = 1) result
+JOIN	details_contract ON details_contract.additional_service_code = result.code
+JOIN	contract ON contract.contract_code = details_contract.contract_code
+JOIN	service_type ON contract.service_code = service_type.service_type_code;
+
+-- 15. Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, 
+-- dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+
+SELECT	result.code,
+		employee.employee_name,
+        education_level.education_level_name,
+        department.department_name,
+        employee.employee_phone,
+        employee.employee_address
+FROM 	employee
+RIGHT JOIN (
+		SELECT	employee_code AS code,
+				count(employee_code)
+        FROM	contract
+        GROUP BY employee_code
+        HAVING 	count(contract.employee_code) = 1) result
+        ON result.code = employee.employee_code
+JOIN	education_level ON education_level.education_level_code = employee.education_level_code
+JOIN	department ON department.department_code = employee.department_code;
